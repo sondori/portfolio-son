11cbc935a35dc41e8aa965af3876f9fbe768a2e7
손혜지 <sonhyeji@sonhyejiui-MacBookAir.local>
2022-10-28 15:23:15 +0900

    4 commit

--- js/index.js
+++ js/index.js
@@ -1,5 +1,14 @@
 $(document).ready(function(){
 
+
+    AOS.init()
+
+    $(".nav ul li").mouseover(function(){
+        let i = $(this).index();
+        $(".nav ul li").removeClass("on").eq(i).addClass("on")
+    })
+
+
     $(".about .title ul li a").click(function(e){
         e.preventDefault();
 
@@ -23,6 +32,54 @@ $(document).ready(function(){
         
         $(".nav").toggleClass("on")
     })
+
+    $(".about .about-event .img").mouseover(function(){
+        let i = $(".about .about-event .img").index(this);
+        $(".about .about-content .about-event .txt").hide().eq(i).show("on")
+        $(".about .about-wrap .about-content img").removeClass("on").eq(i).addClass("on")
+    })
+    $(".about .about-event .img").mouseout(function(){
+        let i = $(".about .about-event .img").index(this);
+        $(".about .about-content .about-event .txt").hide();
+        $(".about .about-wrap .about-content img").removeClass("on")
+    })
+
+
+    $(".skill .skill-event img").mouseover(function(){
+        let i = $(".skill-event img").index(this);
+
+        $(".skill .skill-event img").removeClass("on").eq(i).addClass("on")
+        $(".skill .skill-event .txt").hide().eq(i).show()
+
+    })
+    $(".skill .skill-event img").mouseout(function(){
+        let i = $(".skill-event img").index(this);
+        $(".skill .skill-event img").removeClass("on")
+        $(".skill .skill-event .txt").hide()
+    })
+
+
+
+    $(window).scroll(function(){
+
+    let pos = $(window).scrollTop()
+        // console.log($(".about-wrap").offset().top)
+
+        if(pos >= $(".about-wrap").offset().top-800){
+            $(".about-wrap .about-content img").show()
+        }else{
+            $(".about-wrap .about-content img").hide()
+        }
+
+        $(".nav ul li a").eq(2).click(function(e){
+            e.preventDefault();
+            pos = $(".about-wrap").offset().top
+        })
+
+
+
+    })
+
 
 
 });
