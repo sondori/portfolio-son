12d6fc4e8d1931ffd0d22ad981c931c24815341e
손혜지 <sonhyeji@sonhyejiui-MacBookAir.local>
2022-10-30 01:54:04 +0900

    5 commit

--- js/index.js
+++ js/index.js
@@ -33,17 +33,28 @@ $(document).ready(function(){
         $(".nav").toggleClass("on")
     })
 
-    $(".about .about-event .img").mouseover(function(){
-        let i = $(".about .about-event .img").index(this);
+    $(".about .about-content img").mouseover(function(){
+        let i = $(".about .about-content img").index(this);
         $(".about .about-content .about-event .txt").hide().eq(i).show("on")
         $(".about .about-wrap .about-content img").removeClass("on").eq(i).addClass("on")
     })
-    $(".about .about-event .img").mouseout(function(){
-        let i = $(".about .about-event .img").index(this);
-        $(".about .about-content .about-event .txt").hide();
+    $(".about .about-content").mouseout(function(){
+        let i = $(".about .about-content img").index(this);
+        $(".about .about-content .about-event .txt").hide()
         $(".about .about-wrap .about-content img").removeClass("on")
     })
 
+    // $(window).scroll(function(){
+
+    //     let pos = $(".window-content").scrollTop()
+    //         // console.log($(".about-wrap").offset().top)
+    // console.log(pos)
+    //         if(pos >= $(".window-about .title").offset().top){
+    //             $(".window-content .window-about").show()
+    //         }
+    
+    
+    //     })
 
     $(".skill .skill-event img").mouseover(function(){
         let i = $(".skill-event img").index(this);
@@ -71,14 +82,49 @@ $(document).ready(function(){
             $(".about-wrap .about-content img").hide()
         }
 
-        $(".nav ul li a").eq(2).click(function(e){
-            e.preventDefault();
-            pos = $(".about-wrap").offset().top
-        })
-
 
 
     })
+
+    function skill(id, per) {
+        let bar = new ProgressBar.Line(id,{
+            strokeWidth :3,
+            trailWidth: 3,
+            color: "#000",
+            duration: 5000,
+            step: function(state,circle){
+                circle.setText(Math.round(circle.value()*100)+"%")
+            }
+        })
+        bar.animate(per)
+    }
+
+skill("#html" , 0.8)
+skill("#css" , 0.8)
+skill("#js" , 0.6)
+skill("#ps" , 0.8)
+skill("#ai" , 0.8)
+
+
+$(".work .work-all").hide().eq(0).show();
+$(".work .work-menu ul li").click(function(e){
+    e.preventDefault();
+    
+    let i = $(this).index();
+
+    $(".work .work-all").hide().eq(i).show();
+    $(".work .work-menu ul li").removeClass("on").eq(i).addClass("on")
+})
+
+$(".work .work-menu ul li").click(function(){
+    let i = $(this).index();
+
+    $(".work .work-menu ul li").removeClass("on").eq(i).addClass("on")
+})
+
+
+
+
 
 
 
