cd0bae36f5d7a77d02bed860dbad6e7b58e37a64
sondori <jkhsdm@naver.com>
2022-11-14 16:31:38 +0900

    1114 16:31

--- js/index.js
+++ js/index.js
@@ -125,25 +125,7 @@ $(".work .work-menu ul li").click(function(){
 })
 
 
-"use strict";
-const content = "안녕하세요. 신입 웹디자이너 손혜지입니다."
-const text = document.querySelector(".text")
-let index = 0;
 
-function sleep(delay){ 
-    const start = new Date().getTime(); 
-    while (new Date().getTime() < start + delay); 
-    }
-
-function typing(){
-    text.textContent += content[index++];
-    if(index > content.length){
-        text.textContent = ""
-        index = 0;
-        sleep(3000);
-    }
-}
-setInterval(typing, 200)
 
 
 
